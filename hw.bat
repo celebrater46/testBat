82afe78ebd535e1a0cba09b41d6a98ac0ed7374c
celebrater <tkgm45nbchk@gmail.com>
2023-03-11 09:51:19 +0900

    hw succeeded...!

---
+++ hw.bat
@@ -0,0 +1,10 @@
+@REM hide all commands on CMD
+@echo off
+
+@REM type only the BAT file name to run: "C:\hoge\testbat\hw.bat" or ".\hw.bat"
+echo hello world...!
+
+@REM write %1 as augument
+echo hello world, %1 and %2...!
+
+pause
